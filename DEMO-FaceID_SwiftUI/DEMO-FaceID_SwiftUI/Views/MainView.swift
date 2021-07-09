29f2a71e7d0ef778cfecb18f561ac405a724cff4
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-07-09 14:21:14 +0300

    feature: add SceneDelegate, add Login/Main views

---
+++ DEMO-FaceID_SwiftUI/DEMO-FaceID_SwiftUI/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  DEMO-FaceID_SwiftUI
+//
+//  Created by User on 09.07.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
