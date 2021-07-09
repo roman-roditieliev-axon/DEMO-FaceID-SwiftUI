b8a5975e5a40ece6dc0fdfc56322a4a349b2c774
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-07-09 12:28:35 +0300

    feature: create repo

---
+++ DEMO-FaceID_SwiftUI/DEMO-FaceID_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DEMO-FaceID_SwiftUI
+//
+//  Created by User on 09.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DEMO-FaceID_SwiftUI/DEMO-FaceID_SwiftUI/DEMO_FaceID_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO_FaceID_SwiftUIApp.swift
+//  DEMO-FaceID_SwiftUI
+//
+//  Created by User on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct DEMO_FaceID_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
